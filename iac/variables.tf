053136bf6715699aaed388044d4b6d84e7adacde
suraj10685 <suraj.waghmare@hotmail.com>
2025-02-21 11:48:51 +0530

    Create variables.tf

---
+++ iac/variables.tf
@@ -0,0 +1,11 @@
+variable "rgname" {
+  description = "Resource group name"
+  default = "github-action1"
+  type = string
+}
+
+variable "location" {
+  description = "Azure location"
+  default = "East US"
+  type = string
+}

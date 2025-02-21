d8aa0adb0bcef2862fe0f51c4679f76ac1a521e4
suraj10685 <suraj.waghmare@hotmail.com>
2025-02-21 11:46:13 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,1 +1,14 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "Terraform_Test"
+    storage_account_name = "terraforstr"
+    container_name = "terraform"
+    key = "dev.terraform.tfstate"
+  }
+}
 
+module "RG" {
+  source = "./module/RG"
+  rgname = var.rgname
+  location = var.location
+}

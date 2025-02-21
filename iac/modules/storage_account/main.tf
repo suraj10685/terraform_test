40c9dbc8ba70a77b584679ed61047374681231c0
suraj10685 <suraj.waghmare@hotmail.com>
2025-02-21 15:49:48 +0530

    Update main.tf

--- iac/modules/storage_account/main.tf
+++ iac/modules/storage_account/main.tf
@@ -1,5 +1,7 @@
 resource "azurerm_storage_account" "storage" {
-  name                = var.storage_account_name
-  location            = var.location
-  resource_group_name = var.rgname
+  name                      = var.storage_account_name
+  location                  = var.location
+  resource_group_name       = var.rgname
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
 }

88b5a388e819d1d4f6f606e6cbcfa1223164fab4
suraj10685 <suraj.waghmare@hotmail.com>
2025-02-25 12:49:14 +0530

    Update main.tf

--- iac/modules/search_service/main.tf
+++ iac/modules/search_service/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_search_service" "default" {
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = var.sku
-  semantic_search_sku = var.semantic_search_sku
+#  semantic_search_sku = var.semantic_search_sku
 
   identity {
     type = var.identity_type

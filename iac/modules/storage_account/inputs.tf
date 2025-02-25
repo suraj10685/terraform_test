1e82364dd9e6875ce58d7825f091d4bbec932c97
suraj10685 <suraj.waghmare@hotmail.com>
2025-02-25 12:38:28 +0530

    Create inputs.tf

---
+++ iac/modules/storage_account/inputs.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "storage_account_name" {
+  description = "Storage Account Name"
+  type        = string
+}

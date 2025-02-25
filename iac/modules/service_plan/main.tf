e6f020752dbd2c5e3f057f225740ec8c93d83be5
suraj10685 <suraj.waghmare@hotmail.com>
2025-02-25 17:39:30 +0530

    Update main.tf

--- iac/modules/service_plan/main.tf
+++ iac/modules/service_plan/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_service_plan" "default" {
+resource "azurerm_app_service_plan" "default" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
